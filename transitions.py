2b738a273d784c72130d65172b33ebc915109336
chipdelmal <chipdelmal@gmail.com>
2022-06-11 20:37:40 -0700

    Working more on network

--- transitions.py
+++ transitions.py
@@ -2,6 +2,8 @@
 from os import path
 import pandas as pd
 import numpy as np
+from graph_tool.all import *
+from mpl_chord_diagram import chord_diagram
 import matplotlib.pyplot as plt
 import setup as stp
 
@@ -31,5 +33,32 @@ for ix in range(playNum-1):
 ###############################################################################
 # Plot
 ###############################################################################
-plt.imshow(tMat)
-plt.show()
+# plt.imshow(tMat, vmin=0, vmax=10)
+# plt.show()
+sub = len(arts)
+chord_diagram(
+    tMat[:sub,:sub], # names=arts[:sub], 
+    alpha=.65, pad=.5, gap=0.05, 
+    # use_gradient=True,
+    sorts='size', #'distance',
+    chordwidth=.7,
+    width=0.1, 
+    # directed=False
+    # order=[NAMES.index(i) for i in NAMES]
+)
+plt.savefig(
+    path.join(stp.IMG_PATH, 'artChord.png'),
+    dpi=500, bbox_inches='tight', facecolor='w'
+)
+plt.close('all')
+###############################################################################
+# Graph Tools
+###############################################################################
+g = Graph(directed=True)
+g.add_edge_list(np.transpose(tMat.nonzero()))
+# state = minimize_blockmodel_dl(g)
+state = minimize_nested_blockmodel_dl(g)
+state.draw()
+
+pos = sfdp_layout(g)
+graph_draw(g, pos, output_size=(1000, 1000))
