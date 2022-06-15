ba0bf7a2952db961676596729bb207491decfbb6
chipdelmal <chipdelmal@gmail.com>
2022-06-14 21:11:46 -0700

    Chords with weights on dsk

--- transitions.py
+++ transitions.py
@@ -8,7 +8,7 @@ from mpl_chord_diagram import chord_diagram
 import matplotlib.pyplot as plt
 import setup as stp
 
-(TOP, T_THRESHOLD, P_THRESHOLD) = (100, timedelta(minutes=30), 200)
+(TOP, T_THRESHOLD, P_THRESHOLD) = (650, timedelta(minutes=30), 200)
 (yLo, yHi) = ((1950, 1), (2023, 1))
 yLo = [int(i) for i in yLo]
 yHi = [int(i) for i in yHi]
@@ -127,36 +127,55 @@ for (nme, mat, start, order, cmap) in its:
 ###############################################################################
 # Nested Block Model
 ###############################################################################
+mat = tMat
+# Generate graph --------------------------------------------------------------
 g = Graph(directed=True)
-g.add_edge_list(np.transpose(pMat.nonzero()))
+g.add_edge_list(np.transpose(mat.nonzero()))
+weight = g.new_edge_property("double")
+edges = list(g.edges())
+for e in edges:
+    weight[e] = mat[int(e.source()), int(e.target())]
+# Vertices names --------------------------------------------------------------
 v_prop = g.new_vertex_property("string")
 for (i, v) in enumerate(g.vertices()):
     v_prop[v] = artsTop[i]
-e_prop = g.new_edge_property("string")
-e_size = g.new_edge_property("float")
-for (i, v) in enumerate(g.edges()):
-    e_prop[v] = 'none'
-    e_size = 0.1
+# e_prop = g.new_edge_property("string")
+# e_size = g.new_edge_property("float")
+# for (i, v) in enumerate(g.edges()):
+#     e_prop[v] = 'none'
+#     e_size = 0.1
 # state = minimize_blockmodel_dl(g)
 # state.draw()
 state = minimize_nested_blockmodel_dl(
-    g, state_args=dict(recs=[g.ep.weight], rec_types=["real-exponential"])
+    g, state_args=dict(recs=[weight], rec_types=["real-exponential"])
 )
 mcmc_anneal(
     state, 
-    beta_range=(1, 10), niter=1000, mcmc_equilibrate_args=dict(force_niter=10),
+    beta_range=(1, 2), niter=1000, 
+    mcmc_equilibrate_args=dict(force_niter=10),
     verbose=True
 )
 state.draw(
     # vertex_text=v_prop, 
     # vertex_font_size=3,
     ink_scale=1,
-    edge_pen_width=.2,
     edge_marker_size=0.1,
+    edge_pen_width=prop_to_size(weight, 0.075, 1.5, power=1),
     # edge_marker_size=e_size,
     output=path.join(stp.IMG_PATH, 'NSBM.png'), 
     output_size=(1000, 1000)
 )
+# levels = state.get_levels()
+# levels[3].draw(
+#     # vertex_text=v_prop, 
+#     # vertex_font_size=3,
+#     ink_scale=1,
+#     edge_marker_size=0.1,
+#     # edge_pen_width=prop_to_size(weight, 0.1, 2, power=1),
+#     # edge_marker_size=e_size,
+#     output=path.join(stp.IMG_PATH, 'NSBM.png'), 
+#     output_size=(1000, 1000)
+# )
 ###############################################################################
 # MCMC Posterior Distribution
 ###############################################################################
