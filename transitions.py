CHD Fixing an issue with the color scaling

transitions.py
@@ -15,11 +15,11 @@ import aux as aux
 import setup as stp
 
 if aux.isnotebook():
-    (TOP, WRAN, ID) = (400, 5, 'C') 
+    (TOP, WRAN, ID) = (50, 5, 'C') 
 else:
     (TOP, WRAN, ID) = (int(argv[1]), int(argv[2]), argv[3])
 T_THRESHOLD = timedelta(minutes=60)
-(CVAR, CSCALE) = ('Self', 'Linear')
+(CVAR, CSCALE) = ('Self', 'Log')
 ###############################################################################
 # Read Data
 ###############################################################################
@@ -74,11 +74,11 @@ tMat = aux.calcWeightedTransitionsMatrix(
 )
 # Delete self-loops and normalize ---------------------------------------------
 artDegree = np.sum(tMat, axis=1)+np.sum(tMat, axis=0)
-artDiag = np.diag(tMat, k=0)
+artDiag = np.diag(tMat.copy(), k=0)
 np.fill_diagonal(tMat, 0)
 pMat = aux.normalizeMatrix(tMat)
-
-plt.hist(artDegree)
+# plt.hist(artDegree)
+# plt.yscale('linear')
 ###############################################################################
 # Plot Matrix
 ###############################################################################